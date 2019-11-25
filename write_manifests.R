875d488d16b0e6bb76d99b0e8255f8f820ced28d
Alex Gold <alex.gold@rstudio.com>
2019-11-25 18:32:32 +0000

    Add manifests

---
+++ write_manifests.R
@@ -0,0 +1,7 @@
+# Rewrite manifests
+rsconnect::writeManifest(here::here("App/"))
+rsconnect::writeManifest(here::here("API/"))
+rsconnect::writeManifest(here::here("Model/"))
+rsconnect::writeManifest(here::here("ETL/api_to_db"))
+rsconnect::writeManifest(here::here("ETL/db_to_pin"))
+rsconnect::writeManifest(here::here("ETL/station_api_to_pin"))
