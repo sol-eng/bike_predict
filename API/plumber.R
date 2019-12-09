689b8ab995465ba297048a6f8521bf2548c976a3
Alex Gold <alex.gold@rstudio.com>
2019-12-09 19:11:03 +0000

    Add stuff

--- API/plumber.R
+++ API/plumber.R
@@ -2,12 +2,13 @@ library(plumber)
 library(pins)
 library(tibble)
 library(xgboost)
+library(bikeHelpR)
+library(lubridate)
 
 pins::board_register_rsconnect(server = "https://colorado.rstudio.com/rsc",
                                key = Sys.getenv("RSTUDIOCONNECT_API_KEY"))
-pins::board_connect("rsconnect")
-mod <- pin_get("alex.gold/bike_available_model", board = "rsconnect")
-stats <- pin_get("alex.gold/bike_station_info", board = "rsconnect")
+mods <- list(rxgb = pins::pin_get("alex.gold/bike_model_rxgb", board = "rsconnect"))
+stats <- pins::pin_get("alex.gold/bike_station_info", board = "rsconnect")
 
 
 #* @apiTitle Bike Prediction API
@@ -16,8 +17,9 @@ stats <- pin_get("alex.gold/bike_station_info", board = "rsconnect")
 #* @param station_id the id number of (a) station(s) in the Capitol Bikeshare program
 #* @param max_time time to stop predictions
 #* @param interval prediction interval
+#* @param which which model, defaults to rxgb
 #* @get /pred
-a <- function(station_id, max_time = 86400, interval = 600) {
+function(station_id, max_time = 86400, interval = 600, which = "rxgb") {
   # sanitize inputs
   station_id <- as.numeric(station_id)
   if (!all(station_id %in% stats$station_id)) stop("That station does not exist.")
@@ -27,13 +29,22 @@ a <- function(station_id, max_time = 86400, interval = 600) {
 
   times <- Sys.time() + seq(0, max_time, by = interval)
 
-  df <- tidyr::crossing(times, station_id)
+  df <- tidyr::crossing(times, station_id = as.character(station_id)) %>%
+    left_join(stats) %>%
+    select(-name)
 
-
-  pred_df <- df %>%
-    transmute(station = as.numeric(station_id), hour = lubridate::hour(times)) %>%
-    as.matrix()
+  pred_mat <- df %>%
+    transmute(id = station_id,
+              hour = hour(times),
+              month = month(times),
+              date = date(times),
+              dow = weekdays(times),
+              lat,
+              lon,
+              n_bikes = NA) %>%
+    recipes::bake(mods[[which]]$recipe, .) %>%
+    prep_r_xgb_mat()
 
   df %>%
-    mutate(pred = predict(mod, newdata = pred_df) %>% round())
+    mutate(pred = predict(mods[[which]]$model, newdata = pred_mat) %>% round())
 }
