f682cb931f1579265481090ad15616c642ee7ba8
slopp <sean@rstudio.com>
2020-12-29 22:23:33 +0000

    get the app to work by decreasing data loaded into it

--- App/model_performance/app.R
+++ App/model_performance/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(tidyverse)
 library(pool)
+library(lubridate)
 library(bikeHelpR)
 
 con <- dbPool(odbc::odbc(), dsn = "Content DB")
@@ -9,11 +10,13 @@ pins::board_register_rsconnect(server = "https://colorado.rstudio.com/rsc",
 
 model_details <- pins::pin_get("bike_model_rxgb", board = "rsconnect")
 
+start_date <- today() - dmonths(2)
+
 all_days <- tbl(con, "bike_pred_data") %>%
+    filter(date > start_date) %>%
     collect()
 
-print("data loaded")
-print(nrow(all_days))
+
 
 onStop(function() {
     poolClose(con)
@@ -28,11 +31,12 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            selectInput("data","Train or Test Results?", choices = c("Train", "Test"))
+            selectInput("data","Train or Test Results?", choices = c("Train", "Test")),
+            p(paste0("Showing details for days after: ", start_date))
         ),
         mainPanel(
             p("Model Details:"),
-            textOutput("dets"),
+            tableOutput("dets"),
             tableOutput("summary"),
             tableOutput("metrics"),
             plotOutput("distrib"),
@@ -46,23 +50,28 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     results <- reactive({
         if (input$data == "Test") {
-            all_days %>%
+            res <- all_days %>%
                 filter(date >= model_details$split_date)
         } else {
-            all_days %>%
+            res <- all_days %>%
                 filter(date < model_details$split_date)
         }
+        res
     })
 
-    output$dets <- renderText({
-        capture.output(model_details$model)
+    output$dets <- renderTable({
+        tibble(
+            model = "XGB",
+            num_iterations = model_details$model$niter,
+            features = paste0(model_details$model$feature_names, collapse = ",")
+        )
     })
 
     output$summary <- renderTable({
         tibble(
-            train_date = model_details$train_date,
-            train_window_start = model_details$train_window_start,
-            split_date = model_details$split_date
+            train_date = as.character(model_details$train_date),
+            train_window_start = as.character(model_details$train_window_start),
+            split_date = as.character(model_details$split_date)
         )
     })
 
@@ -98,7 +107,7 @@ server <- function(input, output, session) {
 
     output$resids <- renderPlot(
         results() %>%
-            ggplot(aes(x = resid)) +
+            ggplot(aes(x = residuals)) +
             geom_density() +
             ggtitle("Residual Density Plot") +
             theme_bw() +
